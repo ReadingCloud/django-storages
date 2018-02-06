c028899eade66de69574b32e681fd6ecba6157f6
Eric Meng (Shang Hai Wei Chuang Ruan Jian) <v-wemen@microsoft.com>
2018-02-06 19:03:20 +0800

    修改参数

--- storages/backends/azure_storage.py
+++ storages/backends/azure_storage.py
@@ -45,7 +45,7 @@ class AzureStorage(Storage):
     @property
     def connection(self):
         if self._connection is None:
-            self._connection = BlobService(account_name=self.account_name, account_key=self.account_key, endpoint_suffix="core.chinacloudapi.cn")
+            self._connection = BlobService(account_name=self.account_name, account_key=self.account_key, host_base="core.chinacloudapi.cn")
         return self._connection
 
     @property
